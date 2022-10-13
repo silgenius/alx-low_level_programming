d9a0de75beed6d5cc2acf9ad40804a9dbe6b3e5d
martin olutade <silthegenius@gmail.com>
2022-10-13 16:53:44 -0700

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main description
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n
+			", 60, 1, stderr);
+	return (1);
+}
+
