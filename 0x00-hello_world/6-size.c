e6329a89c499b4fd9c1f12c88b992cc31d3548b2
martin olutade <silthegenius@gmail.com>
2022-10-13 10:29:05 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/** main - description for main
+ *
+ * Return 0
+ */
 int main(void)
 {
 	char i;
@@ -9,11 +13,9 @@ int main(void)
 	float p;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(m));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(m));
 	printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(n));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(o));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(p));
-		
-	return (0);
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(p));		return (0);
 }
 
