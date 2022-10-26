3c34dbabb33c063ec3cb96712c8fe7dfa1706297
martin olutade <silthegenius@gmail.com>
2022-10-25 17:26:10 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - function resets n nto 98
+ * @n: is a  pointer
+ * Return: nothing because it iis a void function
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
