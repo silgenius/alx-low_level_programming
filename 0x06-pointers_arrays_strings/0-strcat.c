1bbfd4763cd1378881dd9bdcf56526e7cc00e328
martin olutade <silthegenius@gmail.com>
2022-10-27 06:49:24 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenate 2 char
+ * @dest: first char
+ * @src: second char
+ *
+ * Return: the result
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
