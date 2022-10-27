29a0218128f49e5cfbde0358e613d16644d5b54e
martin olutade <silthegenius@gmail.com>
2022-10-27 07:32:36 -0700

    commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,16 +2,15 @@
 
 /**
  * _strncpy - copies a string
+ *
  * @dest: the new file for the copied text
  * @src: the copied text
  * @n:the amount of copies to be printed
+ *
  * Return: return the copied text
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-
-	while (i <= n)
-		return (strcpy(dest, src));
+		return (strncpy(dest, src, n));
 }
