4c2e5bc424dc8bf464da7d086826ecf7eefdf374
martin olutade <silthegenius@gmail.com>
2022-10-27 12:54:28 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares 2 string
+ *
+ * @s1: first int
+ * @s2: second int
+ *
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
