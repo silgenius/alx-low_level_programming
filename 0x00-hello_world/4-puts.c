e97d9f8c7008317e0e65c168d27c63d97f0abdb1
silgenius <martinolutade@gmail.com>
2023-08-10 18:33:27 -0700

    added some new file.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - a C program that prints exactly
+ * "Programming is like building a multilingual puzzle,
+ * followed by a new line."
+ *
+ * Description: Using the puts function
+ * to display a string.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
