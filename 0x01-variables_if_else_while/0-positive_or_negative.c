5d7f012482efbdd776f338e9957638cec76e118a
silgenius <martinolutade@gmail.com>
2023-08-11 14:58:02 -0700

    Added some new script

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - check number
- * @n: int to be checked
+ *
  * Return: 0
  */
-int main(signed int n)
+int main(void)
 {
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 		printf("%d is negative\n", n);
 	else if (n == 0)
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints all
+ * possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
