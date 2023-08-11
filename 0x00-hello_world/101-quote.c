45eb25a5918c5cf435fc285ae329276e2779813a
silgenius <martinolutade@gmail.com>
2023-08-10 19:59:06 -0700

    fixed all script with error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - using the putchar to display a string
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	int i = 0;
+
+	while (str[i])
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
