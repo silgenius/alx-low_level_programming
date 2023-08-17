b1f6bdba0030afcb568db58086e5735aa8a5938f
silgenius <martinolutade@gmail.com>
2023-08-16 20:43:34 -0700

    fixed all errors

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry pointt
+ *
+ * Description: A program that computes and
+ * prints the sum of all multiples of 3 and 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int sum = 0;
+
+	for (int i = 3; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
