a742b5e41ab4e111b989168c568c41674399d38e
silgenius <martinolutade@gmail.com>
2023-08-22 20:27:41 -0700

    adde a new file

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * main - a program that generates random valid passwords
+ * for the program 101-crackme
+ *
+ * Return: generated password
+ */
+int main(void)
+{
+	int i, length, random_index;
+	char password[10];
+
+	srand (time(NULL));
+
+	const char *charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#&%!";
+	length = 11;
+
+	for (i = 0; i < length; i++)
+	{
+		random_index = rand() % strlen(charset);
+		password[i] = charset[random_index];
+	}
+	password[i] = '\0';
+	printf("%s\n", password);
+
+	return (0);
+}
