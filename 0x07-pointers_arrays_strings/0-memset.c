e432ded98da0063eaadeb781a4d961b01d7103d9
silgenius <martinolutade@gmail.com>
2023-08-28 12:06:11 -0700

    fixed betty error

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 /**
- * char *_memset(char *s, char b, unsigned int n) - The _memset() function fills
- * the first n bytes of the memory area pointed to by s with the constant byte b
- * 
+ * char *_memset - The _memset() function fills
+ * the first n bytes of the memory area pointed to by s
+ * with the constant byte b
+ *
  * @s: char
  * @b: char (The value to be set)
  * @n: The number of bytes to be set in the memory block
