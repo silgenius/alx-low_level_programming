e680bf3bb101302ea13ea4f4339e4b1a2d2b79ea
silgenius <martinolutade@gmail.com>
2023-08-30 18:55:48 -0700

    fixed compilation error for 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,19 +14,6 @@ int len(char *s)
 }
 
 /**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: The input string.
- *
- * Return: 1 if palindrome, 0 otherwise.
- */
-int is_palindrome(char *s)
-{
-	int length = len(s);
-
-	return (palindrome_checker(length, s));
-}
-
-/**
  * palindrome_checker - Helper function to check if a string is a palindrome.
  * @n: The remaining length to check.
  * @s: The input string.
@@ -40,4 +27,17 @@ int palindrome_checker(int n, char *s)
 	if (*s != s[n - 1])
 		return (0);
 	return (palindrome_checker(n - 2, s + 1));
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The input string.
+ *
+ * Return: 1 if palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	int length = len(s);
+
+	return (palindrome_checker(length, s));
 }
