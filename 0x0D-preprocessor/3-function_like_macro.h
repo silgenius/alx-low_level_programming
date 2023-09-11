680b7e5ee49704cf8fdc7949f86b2f5632fe6319
silgenius <martinolutade@gmail.com>
2023-09-11 05:52:28 -0700

    included solutions to all problem set

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef 3-FUUNCTION_LIKE_MACRO_H
+#define 3-FUUNCTION_LIKE_MACRO_H
+
+#define ABS(x) (-(x));
+
+#endif /* 3-FUUNCTION_LIKE_MACRO_H */
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef 4-SUM_H 
+#define 4-SUM_H
+
+#define SUM (a, b) ((a) + (b))
+
+#endif /* 4-SUM_H */
