1a95351696b035a295450de45bc404756645d3cb
silgenius <martinolutade@gmail.com>
2023-09-12 19:30:15 -0700

    fixed file2 error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 		if (d->age == '\0')
 			printf("Age: (nil)\n");
 		else
-			printf("Age: %7.6f\n", d->age);
+			printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
