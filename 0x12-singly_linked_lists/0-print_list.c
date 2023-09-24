7fae7a7e3d47a374b142a31cc229bd83fef4288c
silgenius <martinolutade@gmail.com>
2023-09-23 17:57:20 -0700

    fixed requirement error for pset 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 
 /**
- * print_list - print all elements of `list_t` list
- * @h: head of linked list
- * Return: number of nodes
+ * print_list - Prints all elements of a linked list.
+ * @h: A pointer to the head of the linked list.
+ *
+ * Description: This function takes a pointer to the head of a linked list
+ *              and prints the elements of the list along with their indexes.
+ *
+ * Return: The number of nodes in the linked list.
  */
 size_t print_list(const list_t *h)
 {
@@ -13,6 +17,9 @@ size_t print_list(const list_t *h)
 
 	c = 0;
 	current = h;
+
+	if (h == NULL)
+		return (0);
 	while (current != NULL)
 	{
 		if (current->str == NULL)
