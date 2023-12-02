0302624d5d8270623bf91a9aa4aa8b323dc52415
martin olutade <112440359+silgenius@users.noreply.github.com>
2023-12-02 04:15:35 +0100

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -19,24 +19,25 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *array;
 
 	x = 0;
+	if (ht == NULL)
+		return;
 	printf("{");
 
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i])
 		{
-			x++;
 			array = ht->array[i];
 			while (array != NULL)
 			{
-				if (x != 1 && ht->array[i - 1] == NULL)
-					printf(", ");
 				printf("\'%s\': \'%s\'", array->key, array->value);
 				array = array->next;
 
-				if (ht->array[i + 1] != NULL && i < ht->size - 1)
-					printf(", ");
+				if (array != NULL)
+					 printf(", ");
 			}
+			if (i < ht->size - 1 && ht->array[i + 1] != NULL)
+				 printf(", ");
 		}
 	}
 
