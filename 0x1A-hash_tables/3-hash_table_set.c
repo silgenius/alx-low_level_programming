3e4187acf3aa6179fdc55239e240c241860d3927
silgenius <olutademartin@gmail.com>
2023-12-01 16:36:11 -0800

    Fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,42 +4,32 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	const unsigned char *unsignedkey = (const unsigned char *)key;
-	hash_node_t *item, *head_ptr;
+	hash_node_t *item;
 
 	if (!key)
 		return (0);
 
 	index = key_index(unsignedkey, ht->size);
-	
-	if (!((ht->array)[index]))
-	{
-		head_ptr = malloc(sizeof(hash_node_t));
-		if (!head_ptr)
-			return (0);
 
-		head_ptr = NULL;
-		ht->array[index] = head_ptr;
-	}
-
-	item = create_item(ht->array[index], key, value);
+	item = create_item(key, value);
 	if (item == NULL)
 		return (0);
+	item->next = ht->array[index];
+	ht->array[index] = item;
 
 	return (1);
 }
 
-hash_node_t *create_item(hash_node_t *head, const char *key, const char *value)
+hash_node_t *create_item(const char *key, const char *value)
 {
 	hash_node_t *ptr = malloc(sizeof(hash_node_t));
 
-	if (!ptr)
+	if (ptr == NULL)
 		return (NULL);
 
 	ptr->key = strdup(key);
 	ptr->value = strdup(value);
-	ptr->next = head;
+	ptr->next = NULL;
 
-	head = ptr;
-
-	return (head);
+	return (ptr);
 }
