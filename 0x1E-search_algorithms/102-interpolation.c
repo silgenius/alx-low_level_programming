c1b028b59df14bba2701efdccbccf60c0e155c34
silgenius <olutademartin@gmail.com>
2024-07-16 09:01:12 -0700

    Implement solution to task

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - Perform interpolation search on an array of integers.
+ * @array: Pointer to the first element of the array.
+ * @size: Number of elements in the array.
+ * @value: Value to search for within the array.
+ *
+ * Return: Index of the element if found, otherwise -1.
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low = 0;
+	size_t high = size - 1;
+	size_t probe;
+
+	probe = low + (((double)(high - low) /
+				(array[high] - array[low])) *
+				(value - array[low]));
+
+	while (value >= array[low] && value <= array[high] && low <= high)
+	{
+		printf("Value checked array[%ld] = [%d]\n", probe, array[probe]);
+		if (array[probe] == value)
+			return (probe);
+
+		else if (array[probe] < value)
+			low = probe + 1;
+		else
+			high = probe - 1;
+		probe = low + (((double)(high - low) /
+				(array[high] - array[low])) *
+				(value - array[low]));
+	}
+
+	if (probe > size)
+		printf("Value checked array[%ld] is out of range\n", probe);
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,6 +7,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 void print_array(int *array, size_t index, size_t end);
 #endif /* SEARCH_ALGOS */
