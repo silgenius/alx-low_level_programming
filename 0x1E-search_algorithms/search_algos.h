7425dee127e98ab97249f2b6dec23d1d6a651870
silgenius <olutademartin@gmail.com>
2024-07-16 11:06:39 -0700

    Update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,8 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
+int binary_search_range(int *array, size_t left, size_t right, int value);
 void print_array(int *array, size_t index, size_t end);
 #endif /* SEARCH_ALGOS */
